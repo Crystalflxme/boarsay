8af65752a7332e4314b1605dadf0ba7948927db7
Paul <43885283+Crystalflxme@users.noreply.github.com>
2024-01-12 19:04:30 -0500

    Revise to not use fs
    Also now has default text if no args supplied

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
 use std::env;
-use std::fs;
-use std::process;
 
 fn main() {
-    let res = fs::read_to_string("src/boar.txt");
+    const SPACING: usize = 2;
+    const BOAR: &str = include_str!("boar.txt");
 
-    if let Ok(boar) = res {
-        const SPACING: usize = 2;
+    let text = {
+        let mut args: Vec<String> = env::args().collect();
+        args.remove(0);
 
-        let text = {
-            let mut args: Vec<String> = env::args().collect();
-            args.remove(0);
+        if args.len() == 0 {
+            "Hello. I am the Boar.".to_string()
+        } else {
             args.join(" ")
-        };
-
-        let display = format!("| {text} |");
-        let line_len = display.len() - SPACING;
-        
-        println!("");
-        println!("/{}\\", "-".repeat(line_len));
-        println!("{display}");
-        println!("\\{}/", "-".repeat(line_len));
-
-        let pt_start = line_len / 2;
-        for i in pt_start..=(pt_start + 2) {
-            println!("{}\\", " ".repeat(i))
         }
+    };
 
-        println!("");
-        println!("{boar}");
-    } else {
-        eprintln!("the boar is a lie");
-        process::exit(1);
+    let display = format!("| {text} |");
+    let line_len = display.len() - SPACING;
+    
+    println!("");
+    println!("/{}\\", "-".repeat(line_len));
+    println!("{display}");
+    println!("\\{}/", "-".repeat(line_len));
+
+    let pt_start = line_len / 2;
+    for i in pt_start..=(pt_start + 2) {
+        println!("{}\\", " ".repeat(i))
     }
+
+    println!("");
+    println!("{BOAR}");
 }
